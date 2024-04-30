3623bc8b088e4ec8c9955c475010020860cbd697
Leo Santos <leo040604@gmail.com>
2024-04-30 09:37:08 -0300

    feat: adding props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import './App.css'
 
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+
+import SayMyName from './components/SayMyName'
+
+import Pessoa from './components/Pessoa'
 
 function App() {
-  const name = 'leonardo'
-  const newName = name.toUpperCase()
+  // const name = 'leonardo'
+  // const newName = name.toUpperCase()
 
-  function sum(a, b) {
-    return a + b
-  }
+  // function sum(a, b) {
+  //   return a + b
+  // }
+
+  const nome = 'leo'
 
   return (
     <div className="App">
-      <h1>Olá React!</h1>
+      {/* <h1>Olá React!</h1>
       <p>Meu primeiro App!</p>
       <p>Olá, {newName}</p>
-      <p>soma: {sum(1, 2)}</p>
-      <HelloWorld />
+      <p>soma: {sum(1, 2)}</p> */}
+      {/* <HelloWorld /> */}
+      <SayMyName nome="Leonardo" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="João"
+        idade="30"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   )
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,5 @@
+function SayMyName(props) {
+  return <div>Fala aí, {props.nome}, suave?</div> //deixa  o componente dinâmico
+}
+
+export default SayMyName
